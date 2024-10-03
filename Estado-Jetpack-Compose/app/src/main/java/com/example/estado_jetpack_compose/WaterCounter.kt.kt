4de7509ae41b1c95a2b0c58008a341ad176dfc2b
ADMIN <s20120189@alumnos.itsur.edu.mx>
2024-10-02 23:42:26 -0600

    Implementacion de rememberSaveable.

--- Estado-Jetpack-Compose/app/src/main/java/com/example/estado_jetpack_compose/WaterCounter.kt.kt
+++ Estado-Jetpack-Compose/app/src/main/java/com/example/estado_jetpack_compose/WaterCounter.kt.kt
@@ -1,5 +1,7 @@
 package com.example.estado_jetpack_compose
+import WellnessTaskItem
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -9,17 +11,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember {mutableStateOf(0)}
-        Text("You've had $count glasses.")
-
+        var count by rememberSaveable { mutableStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
-            Text("Agrega uno")
+            Text("Add one")
         }
     }
 }
