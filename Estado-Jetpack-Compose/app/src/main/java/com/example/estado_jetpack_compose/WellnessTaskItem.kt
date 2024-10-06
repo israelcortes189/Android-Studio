4d8d1e9c6ac695007336a55a104e8210c01b0e9b
ADMIN <s20120189@alumnos.itsur.edu.mx>
2024-10-06 03:01:46 -0600

    Se agregaron estados en el viewModel

--- Estado-Jetpack-Compose/app/src/main/java/com/example/estado_jetpack_compose/WellnessTaskItem.kt
+++ Estado-Jetpack-Compose/app/src/main/java/com/example/estado_jetpack_compose/WellnessTaskItem.kt
@@ -10,26 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-
-@Composable
-fun WellnessTaskItem(
-    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
-) {
-    var checkedState by rememberSaveable { mutableStateOf(false) }
-
-    WellnessTaskItem(
-        taskName = taskName,
-        checked = checkedState,
-        onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = onClose,
-        modifier = modifier,
-    )
-}
 
 @Composable
 fun WellnessTaskItem(
