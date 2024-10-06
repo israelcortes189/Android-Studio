801f5526a7f65158d693850499cae3c1b0f7bf23
ADMIN <s20120189@alumnos.itsur.edu.mx>
2024-10-06 01:55:37 -0600

    Lista de tareas

---
+++ Estado-Jetpack-Compose/app/src/main/java/com/example/estado_jetpack_compose/WellnessTasksList.kt
@@ -0,0 +1,27 @@
+package com.example.estado_jetpack_compose
+
+import WellnessTaskItem
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+
+
