a3dec5559596b192a0564a1938d351185736e20b
ADMIN <s20120189@alumnos.itsur.edu.mx>
2024-10-16 15:24:43 -0600

    Implementacion de Previews para diferentes tamaños de pantallas

--- Disenio Adaptable/basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
+++ Disenio Adaptable/basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
@@ -65,6 +65,26 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true, widthDp = 700)
+@Composable
+fun ReplyAppMediumPreview() {
+    ReplyTheme {
+        Surface {
+            ReplyApp(windowSize = WindowWidthSizeClass.Medium)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 1000)
+@Composable
+fun ReplyAppExpandedPreview() {
+    ReplyTheme {
+        Surface {
+            ReplyApp(windowSize = WindowWidthSizeClass.Expanded)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun ReplyAppCompactPreview() {
