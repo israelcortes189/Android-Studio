134d9aa40179ef0cd0b195cadfe30b80ad53697a
ADMIN <s20120189@alumnos.itsur.edu.mx>
2024-10-16 20:51:25 -0600

    Diseño de contenido Adaptable.

--- Disenio Adaptable/basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/ui/ReplyHomeContent.kt
+++ Disenio Adaptable/basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/ui/ReplyHomeContent.kt
@@ -116,10 +116,8 @@ fun ReplyListAndDetailContent(
         val activity = LocalContext.current as Activity
         ReplyDetailsScreen(
             replyUiState = replyUiState,
-            modifier = Modifier
-                .padding(top = dimensionResource(R.dimen.email_list_item_vertical_spacing))
-                .weight(1f),
-            onBackPressed = {}
+            modifier = Modifier.weight(1f),
+            onBackPressed = { activity.finish() }
         )
     }
 }
